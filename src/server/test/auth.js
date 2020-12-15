c4cf0df9353e27b09e2e82533b237a14235addbc
Nick Ducker <nick_ducker@live.com>
2020-12-16 08:42:26 +1030

    update tests to live in one file and tweak auth route tests

--- src/server/test/auth.js
+++ src/server/test/auth.js
@@ -10,6 +10,19 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
+///// AUTH ROUTE TESTS ///////
+
+after(() => {
+  console.log('dropping db') 
+  User.deleteMany({},(err) => {
+    if (err) {
+      console.log('There was a problem clearing the DB:', err)
+    } else {
+      console.log('DB successfully dropped')
+    }
+  }
+)})
+
 describe("Auth routes", () => {
 
   const userObject = {
@@ -24,17 +37,6 @@ describe("Auth routes", () => {
     "email": "user@email.com",
     "password": "password"
   }
-
-  after(() => {
-    console.log('dropping db') 
-    User.deleteMany({},(err) => {
-      if (err) {
-        console.log('There was a problem clearing the DB:', err)
-      } else {
-        console.log('DB successfully dropped')
-      }
-    }
-  )})
  
   describe('POST /register', () => {
 
@@ -63,9 +65,9 @@ describe("Auth routes", () => {
     })
 
     it('should have inserted a new user into the DB', async () => {
-      const firstUser = await User.find()
-      expect(firstUser).to.be.an('array'),
-      expect(firstUser[0].email).to.equal('user@email.com')
+      const firstUser = await User.findOne({email: 'user@email.com'})
+      expect(firstUser).to.be.an('object'),
+      expect(firstUser.email).to.equal('user@email.com')
     })
   })
 
@@ -110,4 +112,135 @@ describe("Auth routes", () => {
 
   })
 
+})
+
+////////////// USER ROUTE TESTS ///////////////
+
+describe("User routes", () => {
+
+  let userObject = {}
+
+  for (let index = 0; index < 11; index++) {
+    userObject.email = `email${index}@email.com`
+    userObject.name = `username${index}`
+    userObject.password = `password${index}`
+    if (index % 2 == 1) {
+      userObject.is_artist = true
+    }
+
+    new User(userObject).save((err,user) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+  }
+
+  describe("GET /all", () => {
+    it('should return all users', (done) => {
+      chai.request(app)
+        .get('/all')
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+        })
+        done()
+    })
+  })
+
+  describe("GET /allArtists", () => {
+    it('should return only artists tied to user', (done) => {
+      chai.request(app)
+        .get('/allArtists')
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+        })
+        done()
+    })
+  })
+
+  describe("GET /allProducers", () => {
+    it('should return only producers tied to user', (done) => {
+      chai.request(app)
+        .get('/allProducers')
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+        })
+        done()
+    })
+  })
+
+  describe("GET /:id", () => {
+    it('should return only one specific user', (done) => {
+      chai.request(app)
+        .get('/5')
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+        })
+      done()
+    })
+  })
+
+  describe("POST /:id", () => {
+    it('should modify logged in user details and update', (done) => {
+      const newDetails = {
+        name: "new name",
+        email: 'newemail@email.com'
+      }
+
+      chai.request(app)
+        .post('/5')
+        .send(newDetails)
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+          expect(res.user.name).to.equal('new name')
+        })    
+      done()  
+    })
+  })
+
+  describe("POST /:id/company", () => {
+    it('should add a company to a specific user', (done) => {
+      chai.request(app)
+        .post('/5')
+        .send('1')
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+          expect(res.user.companies[0]).to.be.an('object')
+        })
+      done()
+    })
+  })
+
+  describe("DELETE /:id", () => {
+    it('should delete profile of logged in user', (done) => {
+      chai.request(app)
+        .delete('/5')
+        .end((err,res) => {
+          if (err) {
+            console.log(err)
+          }
+          expect(res).to.have.status(200)
+          expect(res.user).to.be(null)
+        })
+      done()
+    })
+  })
+  
 })
