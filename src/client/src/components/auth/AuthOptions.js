02acc14410b50c1415864cd66e8cdd4f35f238c1
Nick Ducker <nick_ducker@live.com>
2020-12-29 13:22:58 +1030

    add dynamic render to auth options

--- src/client/src/components/auth/AuthOptions.js
+++ src/client/src/components/auth/AuthOptions.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import { CurrentUserContext } from '../../store/currentUser'
 
 
 export default function AuthOptions() {
+
+  const {state: currentUserState} = useContext(CurrentUserContext)
+
   return (
     <Nav className='ml-auto'>
-      <Nav.Link as={Link} to='/'>
-        Login
-      </Nav.Link>
-      <Nav.Link as={Link} to='/register'>
-        Sign Up
-      </Nav.Link>
+      {currentUserState.user ? 
+      <>
+        <Nav.Link as={Link} to='/'>
+          Login
+        </Nav.Link>
+        <Nav.Link as={Link} to='/register'>
+          Sign Up
+        </Nav.Link>
+      </>
+      :
+        <Nav.Link as={Link} to='/logout'>
+          Logout
+        </Nav.Link>
+      }
       <Nav.Link as={Link} to='/artists'>
         Artists
       </Nav.Link>
