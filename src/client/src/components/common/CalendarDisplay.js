fc98c31fe68f3356605b22efad5d00048e154bd1
Nick Ducker <nick_ducker@live.com>
2021-01-02 16:27:25 +1030

    fix calendar to accept db shows and format correctly

--- src/client/src/components/common/CalendarDisplay.js
+++ src/client/src/components/common/CalendarDisplay.js
@@ -3,23 +3,28 @@ import {Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment'
 import EventModal from './EventModal'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import $ from 'jquery';
 
 const localizer = momentLocalizer(moment)
 
-const start = new Date(moment().format())
-const end = new Date(moment().add(3, 'hours').format())
+// const start = new Date(moment().format())
+// const end = new Date(moment().add(3, 'hours').format())
 
-const dummyData = [
-  {
-    title: "event 1",
-    start: start,
-    end: end,
-    resources: "This is an event that happens"
-  }
-]
+// const dummyData = [
+//   {
+//     title: "event 1",
+//     start: start,
+//     end: end,
+//     resources: "This is an event that happens"
+//   }
+// ]
 
-const CalendarDisplay = (props) => {
+const CalendarDisplay = ({events}) => {
+  const calendarEvents = events.map((show) => {
+    show.eventStart = moment(show.eventStart).toDate()
+    show.eventEnd = moment(show.eventEnd).toDate()
+    return show
+  })
+
   const modalStateShape = {
     title: "",
     eventStart: "",
@@ -31,10 +36,10 @@ const CalendarDisplay = (props) => {
 
   const displayEventModal = (e) => {
     setModalDetails({
-      title: e.title,
-      eventStart: moment(e.start).format('MMMM Do YYYY, h:mm a'),
-      eventEnd: moment(e.end).format('MMMM Do YYYY, h:mm a'),
-      descrip: e.resources
+      title: e.showName,
+      eventStart: moment(e.eventStart).format('MMMM Do YYYY, h:mm a'),
+      eventEnd: moment(e.eventEnd).format('MMMM Do YYYY, h:mm a'),
+      descrip: e.descrip
     })
     setModalShow(true)
   }
@@ -43,9 +48,12 @@ const CalendarDisplay = (props) => {
     <div>
       <Calendar 
         localizer={localizer}
-        events={dummyData}
-        startAccessor="start"
-        endAccessor="end"
+        events={calendarEvents}
+        startAccessor="eventStart"
+        endAccessor="eventEnd"
+        titleAccessor="showName"
+        resourceAccessor="descrip"
+        allDayAccessor="is_completed"
         style={{ height: 500 }}
         onSelectEvent={displayEventModal}
         dayLayoutAlgorithm='no-overlap'
