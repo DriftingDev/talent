ee5b25eac700c9d624197a421c5c91e3896545a9
Nick Ducker <nick_ducker@live.com>
2021-01-05 16:57:44 +1030

    add edit show modal

--- src/client/src/components/layout/ShowCard.js
+++ src/client/src/components/layout/ShowCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Card} from 'react-bootstrap'
+import {Card, Button} from 'react-bootstrap'
 import moment from 'moment'
 import {useHistory} from 'react-router'
+import ShowEditModal from '../producer/ShowEditModal'
 
 const ShowCard = ({show}) => {
   const history = useHistory()
 
   return(
-    <Card border='info' bg='dark' onClick={() => {history.push(`/show/${show._id}`)}}>
+    <Card border='info' bg='dark' >
       <Card.Title>{show.showName}</Card.Title>
       <Card.Subtitle>@ {moment(show.eventStart).format('h:mm a')}</Card.Subtitle>
       <Card.Text>
@@ -18,6 +19,12 @@ const ShowCard = ({show}) => {
           )
         })}
       </Card.Text>
+      <Card.Footer>
+        <Button onClick={() => {history.push(`/show/${show.showNameSlug}`)}}>
+          All Show times
+        </Button>
+        <ShowEditModal showObject={show}/>
+      </Card.Footer>
     </Card>
   )
 }
