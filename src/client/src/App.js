a29323fdb25f1506580e0db21905e3b8f3208f3e
Nic Meg <nicmeg54@gmail.com>
2021-01-06 14:46:16 +1100

    Update App.js
    Fixed routing issue where two components were rendering on same page. Additional switch added.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -10,7 +10,9 @@ import Companies from './components/common/Companies';
 import AllShows from './components/common/AllShows';
 import CreateShows from './components/producer/CreateShows';
 import RegisterArtist from './components/producer/RegisterArtist';
+import DisplayArtist from './components/producer/DisplayArtist';
 import DisplayShow from './components/common/DisplayShow';
+import NotFound from './components/common/NotFound';
 //Router
 import { Route, Switch } from 'react-router-dom';
 import Artists from './components/producer/Artists';
@@ -40,14 +42,21 @@ function App() {
           <Route path='/register' component={Register} exact />
           <PrivateRoutes>
             <DataProvider>
-              <Route path='/companies' component={Companies} exact />
-              <Route path='/calendar' component={Calendar} exact />
-              <Route path='/shows' component={AllShows} exact />
-              <Route path='/shows/create' component={CreateShows} exact />
-              <Route path='/shows/:slug' component={DisplayShow} exact />
-              <Route path='/artists' component={Artists} exact />
-              <Route path='/artists/create' component={RegisterArtist} exact />
-              {/* <Route path='/artists/:id' component={Artists} /> */}
+              <Switch>
+                <Route path='/companies' component={Companies} exact />
+                <Route path='/calendar' component={Calendar} exact />
+                <Route path='/shows' component={AllShows} exact />
+                <Route path='/shows/create' component={CreateShows} exact />
+                <Route path='/shows/:slug' component={DisplayShow} exact />
+                <Route path='/artists' component={Artists} exact />
+                <Route
+                  path='/artists/create'
+                  component={RegisterArtist}
+                  exact
+                />
+                <Route path='/artists/:id' component={DisplayArtist} exact />
+                {/* <Route path='*' component={NotFound} /> */}
+              </Switch>
             </DataProvider>
           </PrivateRoutes>
         </Switch>
