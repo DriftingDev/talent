baa44a2d95f65491aeb844bc38ecf5893cec3afd
Nick Ducker <nick_ducker@live.com>
2021-01-07 15:42:32 +1030

    better dynamic rendering for navbar

--- src/client/src/components/auth/AuthOptions.js
+++ src/client/src/components/auth/AuthOptions.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router'
 import { Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CurrentUserContext } from '../../store/currentUser';
 
 export default function AuthOptions() {
 
   const history = useHistory()
 
+  const {state: currentUserState} = useContext(CurrentUserContext)
+
   return (
+    <>
+    { !currentUserState.user &&
     <Nav className='ml-auto'>
       <Nav.Link onClick={() => {history.push('/')}}>
         Login
@@ -16,5 +21,7 @@ export default function AuthOptions() {
         Sign Up
       </Nav.Link>
     </Nav>
+    }
+    </>
   );
 }
