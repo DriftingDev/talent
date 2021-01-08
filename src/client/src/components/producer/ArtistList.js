291e09021b48ef2b4c1dae5717a501eb2e538caf
Nic Meg <nicmeg54@gmail.com>
2021-01-08 14:51:06 +1100

    Update Artist Layout
    Consistent left and right padding

--- src/client/src/components/producer/ArtistList.js
+++ src/client/src/components/producer/ArtistList.js
@@ -4,49 +4,57 @@ import { useHistory } from 'react-router';
 // import artists from '../../data/artists'
 import { CompanyContext } from '../../store/company';
 import { UsersContext } from '../../store/user';
-import Loading from '../layout/Loading'
+import Loading from '../layout/Loading';
 import ArtistCard from './ArtistCard';
 
 function ArtistList() {
+  const history = useHistory();
 
-  const history = useHistory()
+  let artists;
 
-  let artists
-
-  const { state: CompanyState, fetchCurrentCompany } = useContext(CompanyContext)
+  const { state: CompanyState, fetchCurrentCompany } = useContext(
+    CompanyContext
+  );
 
   useEffect(() => {
-    if(CompanyState.currentCompany === null && localStorage.getItem('currentCompany')){
-      fetchCurrentCompany()
-    } else if (CompanyState.currentCompany === null && !localStorage.getItem('currentCompany')) {
-      history.push('/companies')
+    if (
+      CompanyState.currentCompany === null &&
+      localStorage.getItem('currentCompany')
+    ) {
+      fetchCurrentCompany();
+    } else if (
+      CompanyState.currentCompany === null &&
+      !localStorage.getItem('currentCompany')
+    ) {
+      history.push('/companies');
     }
-  },[CompanyState])
+  }, [CompanyState]);
 
-  if(CompanyState.currentCompany != null){
-    artists = CompanyState.currentCompany.users
-              .filter(user => user.is_artist)
+  if (CompanyState.currentCompany != null) {
+    artists = CompanyState.currentCompany.users.filter(
+      (user) => user.is_artist
+    );
   }
 
   return (
     <>
-      {CompanyState.currentCompany != null ?
-      <Col>
-        <Row>
-          {artists.length > 0 ?
-          artists.map((artist) => (
-            <Col sm={12} md={6} lg={4} xl={4} key={artist.id}>
-              <ArtistCard artist={artist} />
-            </Col>
-          ))
-          :
-          <h2>No artists have been added to this company yet.</h2>
-          }
+      {CompanyState.currentCompany != null ? (
+        <Col className='noPadding'>
+          <Row>
+            {artists.length > 0 ? (
+              artists.map((artist) => (
+                <Col sm={12} md={6} lg={4} xl={4} key={artist.id}>
+                  <ArtistCard artist={artist} />
+                </Col>
+              ))
+            ) : (
+              <h2>No artists have been added to this company yet.</h2>
+            )}
           </Row>
-      </Col>
-      :
-      <Loading />
-      }
+        </Col>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 }
--- src/client/src/components/producer/Artists.js
+++ src/client/src/components/producer/Artists.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 // import Link from 'react-router-dom'
-import { Button, Container, Row } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 // import artists from '../data/artists'
 import NavBar from '../layout/NavBar';
 import ArtistList from './ArtistList';
@@ -8,34 +8,35 @@ import { useHistory } from 'react-router';
 import { CurrentUserContext } from '../../store/currentUser';
 
 const Artists = () => {
-
   const history = useHistory();
 
-  const { state: CurrentUserState } = useContext(CurrentUserContext)
+  const { state: CurrentUserState } = useContext(CurrentUserContext);
 
   useEffect(() => {
-    if(CurrentUserState.user.is_artist){
-      history.push('/shows')
+    if (CurrentUserState.user.is_artist) {
+      history.push('/shows');
     }
-  })
+  });
 
   return (
     <>
       <NavBar />
       <Container>
         <Row className='d-flex justify-content-center'>
-          <h1>Artists</h1>
+          <Col xs={6} className='pt-2'>
+            <h1>Artists</h1>
+          </Col>
+          <Col xs={6} className='pt-2'>
+            <Button
+              onClick={() => {
+                history.push('/artists/create');
+              }}
+              className='float-right'
+            >
+              Create Artist
+            </Button>
+          </Col>
         </Row>
-
-        <Button
-          onClick={() => {
-            history.push('/artists/create');
-          }}
-          block
-        >
-          Create Artist
-        </Button>
-
         <ArtistList />
       </Container>
     </>
