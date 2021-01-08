7ef2ddb630176e021c97b366b1522db0521154ce
Nic Meg <nicmeg54@gmail.com>
2021-01-08 11:17:21 +1100

    Venue Card Styling WIP

--- src/client/src/components/common/AllVenues.js
+++ src/client/src/components/common/AllVenues.js
@@ -1,59 +1,69 @@
-import React, { useContext, useEffect } from 'react'
-import { useHistory } from 'react-router'
-import {CurrentUserContext} from '../../store/currentUser'
-import {CompanyContext} from '../../store/company'
-import {VenueContext} from '../../store/venue' 
-import Loading from '../layout/Loading'
-import { Button, Col, Container, Row } from 'react-bootstrap'
-import VenueAccordion from './VenueAccordion'
-import NavBar from '../layout/NavBar'
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { CurrentUserContext } from '../../store/currentUser';
+import { CompanyContext } from '../../store/company';
+import { VenueContext } from '../../store/venue';
+import Loading from '../layout/Loading';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import VenueAccordion from './VenueAccordion';
+import NavBar from '../layout/NavBar';
 
 const AllVenues = () => {
-
-  const history = useHistory()
-  const { state: CurrentUserState } = useContext(CurrentUserContext)
-  const { state: CompanyState, fetchCurrentCompany } = useContext(CompanyContext)
-  const { state: VenueState, getVenuesByCompany, getVenuesByUser } = useContext(VenueContext)
+  const history = useHistory();
+  const { state: CurrentUserState } = useContext(CurrentUserContext);
+  const { state: CompanyState, fetchCurrentCompany } = useContext(
+    CompanyContext
+  );
+  const { state: VenueState, getVenuesByCompany, getVenuesByUser } = useContext(
+    VenueContext
+  );
 
   useEffect(() => {
-    if(!localStorage.getItem('currentCompany')){
-      history.push('/companies')
+    if (!localStorage.getItem('currentCompany')) {
+      history.push('/companies');
     }
-    if(CompanyState.currentCompany === null) {
-      fetchCurrentCompany()
+    if (CompanyState.currentCompany === null) {
+      fetchCurrentCompany();
     }
-    if(!VenueState.loaded){
-      CurrentUserState.user.is_artist ? getVenuesByUser() : getVenuesByCompany()
+    if (!VenueState.loaded) {
+      CurrentUserState.user.is_artist
+        ? getVenuesByUser()
+        : getVenuesByCompany();
     }
-  },[VenueState, CompanyState])
+  }, [VenueState, CompanyState]);
 
-  return(
+  return (
     <>
       <NavBar />
       <Container bg='dark' fluid>
-        { VenueState.loaded ?
-        <>
-        <Row>
-          <Col>
-            <h1>Venues</h1>
-          </Col>
-          {!CurrentUserState.user.is_artist && 
-          <Button onClick={()=>{history.push('/venues/create')}}>
-            Create Venues
-          </Button>
-          }
-        </Row>
-        <Row>
-          <VenueAccordion venues={VenueState.venues}/>
-        </Row>
-        </>
-        :
-        <Loading />
-        }
+        {VenueState.loaded ? (
+          <>
+            <Col>
+              <Row>
+                <Col>
+                  <h1>Venues</h1>
+                </Col>
+                {!CurrentUserState.user.is_artist && (
+                  <Button
+                    onClick={() => {
+                      history.push('/venues/create');
+                    }}
+                  >
+                    Create Venues
+                  </Button>
+                )}
+              </Row>
+              <Row>
+                <VenueAccordion venues={VenueState.venues} />
+              </Row>
+            </Col>
+          </>
+        ) : (
+          <Loading />
+        )}
       </Container>
     </>
-  )
+  );
+};
 
-}
-
-export default AllVenues
+export default AllVenues;
