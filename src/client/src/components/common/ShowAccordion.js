81ec3d0cbe55dd462b7abc8d3c92f3e5c23300a4
Nic Meg <nicmeg54@gmail.com>
2021-01-11 15:06:42 +1100

    Update ShowAccordion.js

--- src/client/src/components/common/ShowAccordion.js
+++ src/client/src/components/common/ShowAccordion.js
@@ -1,52 +1,43 @@
-import React from 'react'
-import {Accordion, Card, Button, Badge} from 'react-bootstrap'
-import ShowCard from '../layout/ShowCard'
-import moment from 'moment'
+import React from 'react';
+import { Accordion, Card, Button, Badge } from 'react-bootstrap';
+import ShowCard from '../layout/ShowCard';
+import moment from 'moment';
 
-const ShowAccordion = ({shows, showOptions}) => {
-
-  var groupBy = function(xs, key) {
-    return xs.reduce(function(rv, x) {
+const ShowAccordion = ({ shows, showOptions }) => {
+  var groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
       (rv[x[key]] = rv[x[key]] || []).push(x);
       return rv;
     }, {});
   };
 
-  let showsToMap =
-    shows.map((show) => {
-      show.accordionDate = moment(show.eventStart).format("MMMM Do YYYY")
-      return show
-    })
-  
-  showsToMap = groupBy(showsToMap,'accordionDate')
-  const AccordionShows = Object.keys(showsToMap)
-    .map((date, index) => {
-      return (
-        <Card key={index}>
-          <Card.Header >
-            <Accordion.Toggle as={Button}  eventKey={index + 1}>
-              {date}
-            </Accordion.Toggle>
-            {/* <Badge>{showsToMap[date].length}</Badge> */}
-          </Card.Header>
-          <Accordion.Collapse eventKey={index + 1}>
-            <Card.Body>
-              {showsToMap[date].map((show,index) => (
-                <ShowCard key={index} show={show} showOptions={showOptions}/>
-              )
-              )}
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      )
-    })
+  let showsToMap = shows.map((show) => {
+    show.accordionDate = moment(show.eventStart).format('MMMM Do YYYY');
+    return show;
+  });
 
-  return (
-    <Accordion defaultActiveKey="1">
-      {AccordionShows}
-    </Accordion>
-  )
+  showsToMap = groupBy(showsToMap, 'accordionDate');
+  const AccordionShows = Object.keys(showsToMap).map((date, index) => {
+    return (
+      <Card key={index}>
+        <Card.Header>
+          <Accordion.Toggle as={Button} eventKey={index + 1}>
+            {date}
+          </Accordion.Toggle>
+          {/* <Badge>{showsToMap[date].length}</Badge> */}
+        </Card.Header>
+        <Accordion.Collapse eventKey={index + 1}>
+          <Card.Body className='accordian-custom'>
+            {showsToMap[date].map((show, index) => (
+              <ShowCard key={index} show={show} showOptions={showOptions} />
+            ))}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    );
+  });
 
-}
+  return <Accordion defaultActiveKey='1'>{AccordionShows}</Accordion>;
+};
 
-export default ShowAccordion
+export default ShowAccordion;
