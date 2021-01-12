2dd9884203fc3e596b534b7338ff4d18cad6bada
Nick Ducker <nick_ducker@live.com>
2021-01-13 10:21:53 +1030

    add icons and tab layout to artist and producer nav

--- src/client/src/components/layout/ArtistOptions.js
+++ src/client/src/components/layout/ArtistOptions.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import { useHistory } from 'react-router'
 import { Nav, Navbar } from 'react-bootstrap';
+import { BsPersonLinesFill } from 'react-icons/bs'
+import { IoMdMicrophone } from 'react-icons/io'
+import { GiFamilyHouse } from 'react-icons/gi'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ArtistOptions = () => {
 
   const history = useHistory()
 
+  const options = [
+    [<IoMdMicrophone />,"Shows"], 
+    [<GiFamilyHouse />,"Venues"],
+    [<BsPersonLinesFill />,"Team"]]
+
+    const links = options.map((opt) => {
+      return (
+      history.location.pathname.slice(1, opt[1].length + 1) === opt[1].toLowerCase() ?
+      <Nav.Link active onClick={() => {history.push(`/{${opt[1].toLowerCase()}}`)}}>
+        {opt[0]}{" "}{opt[1]}
+      </Nav.Link>
+      :
+      <Nav.Link onClick={() => {history.push(`/${opt[1].toLowerCase()}`)}}>
+        {opt[0]}{" "}{opt[1]}
+      </Nav.Link>
+      )
+    })
+
   return (
     <Navbar fixed='bottom' bg='dark' variant='dark'>
-      <Nav className='ml-auto'>
-        <Nav.Link onClick={() => {history.push('/shows')}}>
-          Shows
-        </Nav.Link>
-        <Nav.Link onClick={() => {history.push('/venues')}}>
-          Venues
-        </Nav.Link>
-        <Nav.Link onClick={() => {history.push('/team')}}>
-          Team
-        </Nav.Link>
+      <Nav  variant="tabs" className='ml-auto'>
+        {links}
       </Nav>
     </Navbar>
   );
--- src/client/src/components/layout/ProducerOptions.js
+++ src/client/src/components/layout/ProducerOptions.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { useHistory } from 'react-router'
 import { Nav, Navbar } from 'react-bootstrap';
+import { BsCalendar, BsPersonLinesFill } from 'react-icons/bs'
+import { IoMdMicrophone } from 'react-icons/io'
+import { GiFamilyHouse } from 'react-icons/gi'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ProducerOptions = () => {
 
   const history = useHistory()
 
+  const options = [
+    [<BsCalendar />, "Calendar"], 
+    [<IoMdMicrophone />,"Shows"], 
+    [<BsPersonLinesFill />,"Artists"], 
+    [<GiFamilyHouse />,"Venues"]]
+
+  const links = options.map((opt) => {
+    return (
+    history.location.pathname.slice(1, opt[1].length + 1) === opt[1].toLowerCase() ?
+    <Nav.Link active onClick={() => {history.push(`/{${opt[1].toLowerCase()}}`)}}>
+      {opt[0]}{" "}{opt[1]}
+    </Nav.Link>
+    :
+    <Nav.Link onClick={() => {history.push(`/${opt[1].toLowerCase()}`)}}>
+      {opt[0]}{" "}{opt[1]}
+    </Nav.Link>
+    )
+  })
+
   return (
     <Navbar fixed='bottom' bg='dark' variant='dark'>
-      <Nav className='ml-auto'>
-        <Nav.Link onClick={() => {history.push('/calendar')}}>
+      <Nav variant='tabs' className='ml-auto'>
+        {links}
+        {/* <Nav.Link onClick={() => {history.push('/calendar')}}>
           Calendar
         </Nav.Link>
         <Nav.Link onClick={() => {history.push('/shows')}}>
@@ -21,7 +44,7 @@ const ProducerOptions = () => {
         </Nav.Link>
         <Nav.Link onClick={() => {history.push('/venues')}}>
           Venues
-        </Nav.Link>
+        </Nav.Link> */}
       </Nav>
     </Navbar>
   );
