ea49a7295fb15bd83a32e5d5857ba3e0afb103ee
Nick Ducker <nick_ducker@live.com>
2021-01-18 22:20:35 +1030

    add link func and flow to artist card

--- src/client/src/components/producer/ArtistCard.js
+++ src/client/src/components/producer/ArtistCard.js
@@ -3,6 +3,7 @@ import { Card, Col, Row } from 'react-bootstrap';
 import { BsEnvelope } from 'react-icons/bs';
 import { useHistory } from 'react-router-dom';
 import { FiPhone } from 'react-icons/fi';
+import { HiLink } from 'react-icons/hi'
 import { ShowContext } from '../../store/show';
 import Loading from '../layout/Loading';
 import moment from 'moment';
@@ -67,9 +68,12 @@ const ArtistCard = ({ artist }) => {
               <a href={`mailto:${artist.email}`}> {artist.email}</a>
             </Card.Text>
             <Card.Text>
-              <FiPhone /> {artist.contact}
+              <FiPhone />: {artist.contact || "N/A"}
             </Card.Text>
-            <Card.Text>{artist.fringe_link}</Card.Text>
+            <Card.Text>
+              {console.log(artist)}
+              <HiLink />: {artist.link ? <span className='btn-link' onClick={() => {window.open("http://" + artist.link)}}>{artist.link}</span> : "N/A"}
+            </Card.Text>
           </Col>
         </Row>
       </Card.Body>
