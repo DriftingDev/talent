1d5b604525748e7d7069b628238d87870762bb58
Nic Meg <nicmeg54@gmail.com>
2021-01-18 19:51:46 +1100

    Update ShowCard.js

--- src/client/src/components/layout/ShowCard.js
+++ src/client/src/components/layout/ShowCard.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import moment from 'moment';
-import { BsPersonLinesFill } from 'react-icons/bs'
-import { GiFamilyHouse } from 'react-icons/gi'
-import { IoMdMicrophone } from 'react-icons/io'
+import { BsPersonLinesFill } from 'react-icons/bs';
+import { GiFamilyHouse } from 'react-icons/gi';
+import { IoMdMicrophone } from 'react-icons/io';
 import { useHistory } from 'react-router';
 import ShowEditModal from '../producer/ShowEditModal';
 import DeleteModal from '../common/DeleteModal';
@@ -13,51 +13,67 @@ import { CurrentUserContext } from '../../store/currentUser';
 const ShowCard = ({ show, showOptions }) => {
   const history = useHistory();
 
-  const { dispatch: showDispatch, deleteShow } = useContext(ShowContext)
-  const { state: currentUserState } = useContext(CurrentUserContext)
+  const { dispatch: showDispatch, deleteShow } = useContext(ShowContext);
+  const { state: currentUserState } = useContext(CurrentUserContext);
 
   return (
-    <Card border='info' bg='dark' className='px-3 py-2 mb-2'>
-      <Card.Title className="font-weight-bold d-flex justify-content-between">
-      {showOptions.withTitle && 
-      <div><IoMdMicrophone /> {show.showName}</div>}
+    <Card border='info' bg='dark' className='px-3 py-2 mb-2 white'>
+      <Card.Title className='font-weight-bold d-flex justify-content-between'>
+        {showOptions.withTitle && (
+          <div>
+            <IoMdMicrophone /> {show.showName}
+          </div>
+        )}
 
-      { showOptions.withAllShowsLink && 
-        <>
-        <Button variant={'info'} onClick={() => {history.push(`/shows/${show.showNameSlug}`)}}>
-          All show times
-        </Button>
-        
-        </>
-      }
-      { (showOptions.withDeleteEdit && !currentUserState.user.is_artist) &&
-        <span>
-        <DeleteModal object={show} name='show' deleteFunc={deleteShow} dispatch={() => {showDispatch({type: "clearShows"})}} />
-        <ShowEditModal showObject={show}/>
-        </span>
-      }
+        {showOptions.withAllShowsLink && (
+          <>
+            <Button
+              variant={'info'}
+              onClick={() => {
+                history.push(`/shows/${show.showNameSlug}`);
+              }}
+            >
+              All show times
+            </Button>
+          </>
+        )}
+        {showOptions.withDeleteEdit && !currentUserState.user.is_artist && (
+          <span>
+            <DeleteModal
+              object={show}
+              name='show'
+              deleteFunc={deleteShow}
+              dispatch={() => {
+                showDispatch({ type: 'clearShows' });
+              }}
+            />
+            <ShowEditModal showObject={show} />
+          </span>
+        )}
       </Card.Title>
-      
+
       {showOptions.withTime && (
-        <Card.Subtitle className="font-weight-bold">
+        <Card.Subtitle className='font-weight-bold'>
           From: {moment(show.eventStart).format(' h:mm a')}
         </Card.Subtitle>
       )}
-      
+
       {showOptions.withEndTime && (
         <>
-        <hr></hr>
-        <Card.Subtitle className="font-weight-bold">
-          Until: {moment(show.eventEnd).format('h:mm a, MMMM Do')}
-        </Card.Subtitle>
+          <hr></hr>
+          <Card.Subtitle className='font-weight-bold'>
+            Until: {moment(show.eventEnd).format('h:mm a, MMMM Do')}
+          </Card.Subtitle>
         </>
       )}
-      
+
       <hr></hr>
       {showOptions.withVenue && (
         <div>
-          <span className="font-weight-bold"><GiFamilyHouse /> Venue:</span> {show.venue.name}{' '}
-          {show.venue.address && '@ ' + show.venue.address}
+          <span className='font-weight-bold'>
+            <GiFamilyHouse /> Venue:
+          </span>{' '}
+          {show.venue.name} {show.venue.address && '@ ' + show.venue.address}
         </div>
       )}
       {showOptions.withArtists &&
@@ -69,12 +85,14 @@ const ShowCard = ({ show, showOptions }) => {
                 history.push(`/artists/${artist._id}`);
               }}
             >
-              <span className="font-weight-bold"><BsPersonLinesFill /> Artist {index + 1}:</span> {artist.accname}
+              <span className='font-weight-bold'>
+                <BsPersonLinesFill /> Artist {index + 1}:
+              </span>{' '}
+              {artist.accname}
             </div>
           );
         })}
       {showOptions.withDescrip && show.descrip && <p>{show.descrip}</p>}
-      
     </Card>
   );
 };
