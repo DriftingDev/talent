9a878bb7ee3ebaeb4eaa7929c05735e8ffe7771c
Nick Ducker <nick_ducker@live.com>
2021-01-18 22:47:40 +1030

    update artist next show

--- src/client/src/components/artist/ArtistNextShow.js
+++ src/client/src/components/artist/ArtistNextShow.js
@@ -1,31 +1,34 @@
 import React from 'react'
 import moment from 'moment'
+import { useHistory } from 'react-router'
 import { Alert, Row, Col} from 'react-bootstrap'
 import { BiTime } from 'react-icons/bi'
 import { BsHouseDoor } from "react-icons/bs"
+import { IoMdMicrophone } from 'react-icons/io'
 
 const ArtistNextShow = ({shows}) => {
   const nextShows = shows.filter((show) => {
     return moment() < moment(show.eventEnd)
   }).sort((a, b) => a.eventStart > b.eventStart)
 
-  
+  const history = useHistory()
+
   return (
     <>
     <Alert variant='danger' className='py-3'>
     { nextShows.length > 0 ?
       <>
         <Row>
-          <Col className="d-flex justify-content-center">NEXT SHOW:</Col>
-          <Col className="d-flex justify-content-center">{nextShows[0].showName}</Col>
+          
+          <Col className="d-flex justify-content-center"><h4 className="text-dark" onClick={()=>{history.push(`shows/${nextShows[0].showNameSlug}`)}}><IoMdMicrophone/><u>NEXT SHOW:</u> {nextShows[0].showName}</h4></Col>
         </Row>
         <hr/>
         <Row>
-          <Col className="d-flex justify-content-center"><BiTime/>{moment(nextShows[0].eventStart).format('MMMM Do YYYY @ h:mm:ss a')} </Col>
+          <Col className="d-flex justify-content-center"><h4 className="text-dark"><BiTime/>{" "}{moment(nextShows[0].eventStart).format('MMMM Do YYYY @ h:mm a')} </h4></Col>
         </Row>
         <hr/>
         <Row>
-          <Col className="d-flex justify-content-center"><BsHouseDoor/>{nextShows[0].venue.name} </Col>
+          <Col className="d-flex justify-content-center"><h4 className="text-dark" onClick={()=>{history.push(`venues/`)}}><BsHouseDoor/>{" "}{nextShows[0].venue.name}</h4> </Col>
         </Row> 
       </>
       :
